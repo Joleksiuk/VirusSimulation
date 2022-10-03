0fc1c1a2ccd94f20ffe9d22918ac0c000220db07
Joleksiuk <julkaoleksiuk26@gmail.com>
2022-10-03 22:17:14 +0200

    Cors update

---
+++ src/main/java/com/pl/virussimulation/MySecurityConfig.java
@@ -0,0 +1,26 @@
+package com.pl.virussimulation;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@AllArgsConstructor
+
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api1/**").permitAll()
+                .antMatchers("/api2/**").permitAll()
+                .antMatchers("/api3/**").permitAll();
+
+    }
+}
+
